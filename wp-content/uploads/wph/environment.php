9d53f40a58916314a9e96c8d8155b0521ce19dd0
Reynaldo Dalog <64511429+reynaldodalog7@users.noreply.github.com>
2021-10-16 14:37:44 +0800

    October162021

--- wp-content/uploads/wph/environment.php
+++
@@ -1,3 +0,0 @@
-<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-$environment_variable = '{"themes":{"1":{"themes_url":"http:\/\/reynaldodalog.com\/wp-content\/themes","use_child_theme":false,"main":{"folder_name":"ryancv","mapped_name":""}}},"allowed_paths":["\/home\/sc1ceehftd4z\/public_html\/wp-content\/themes"],"wordpress_directory":"","site_relative_path":"\/"}'; ?>
